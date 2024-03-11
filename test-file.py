f3787ac70343607f178381115a8b57cc9487cfce
official-anato <139602951+official-anato@users.noreply.github.com>
2024-03-11 23:12:25 +0800

    Obsolete File

--- test-file.py
+++ test-file.py
@@ -1,5 +1,1 @@
-import warp as w
-root = w.App()
-# You can add any code here if you want.
-# This is just a simple file to show how many lines it takes to make an empty window.
-root.run()
+This is another obsolete file, go to test.py instead.
